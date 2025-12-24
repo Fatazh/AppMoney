e683de36ad0ce3678af05555e2404cfeed1cf49f
LAPTOP-C89085J0\2080 <majidilkhithar@hotmail.com>
2025-12-24 11:49:19 +0700

    p

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,14 +1,10 @@
-// server/utils/prisma.ts
-import { PrismaClient } from "../../app/generated/prisma/client";
+import { PrismaClient } from "../../app/generated/prisma/wasm";
+
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg"; // <--- Wajib import ini
+import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL;
-
-// 1. Buat Pool koneksi terlebih dahulu
 const pool = new Pool({ connectionString });
-
-// 2. Masukkan pool ke dalam adapter
 const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
