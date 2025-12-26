e2c57c4aeace7619ac382619da0d4fb40a036805
LAPTOP-C89085J0\2080 <majidilkhithar@hotmail.com>
2025-12-26 23:06:08 +0700

    kklp

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,13 @@
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
+// prisma.config.ts
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
   datasource: {
-    url: process.env.DATABASE_URL,
-    shadowDatabaseUrl: process.env.DIRECT_URL,
+    url: env("DATABASE_URL"), 
   },
 });
